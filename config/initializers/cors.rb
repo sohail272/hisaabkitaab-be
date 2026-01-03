b211d2d710c52d03a4d127b9a2d5bd5bbeed5034
Abdul Sohail <sohailshaikh272@gmail.com>
2026-01-04 00:33:16 +0530

    update cors to support prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://192.168.1.4:5173"
+    origins "http://localhost:5173", "https://hisaabkitaab-fe.onrender.com"
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head]
